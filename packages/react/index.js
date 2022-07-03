4e136a0aa00b153b8d4dc043640696c8f3a9871a
sudongyu <976499226@qq.com>
2022-07-03 11:19:56 +0800

    feat: add react lint config

---
+++ packages/react/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    '@sudongyuer/eslint-config-ts',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'jsx-quotes': [
+      'error',
+      'prefer-double',
+    ],
+    'react/react-in-jsx-scope': 'off',
+  },
+}
